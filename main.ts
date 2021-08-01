32cafba902cba63ec0269832c75e98ad245b9939
nniguel-angel <88258216+nniguel-angel@users.noreply.github.com>
2021-07-31 20:32:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,108 @@
+basic.showLeds(`
+. . # . .
+. # # . .
+# # # # #
+. # # . .
+. . # . .
+`)
+basic.clearScreen()
+basic.pause(500)
+basic.showLeds(`
+. . # . .
+. . # . .
+# # # # #
+. # # # .
+. . # . .
+`)
+basic.clearScreen()
+basic.pause(500)
+for (let index = 0; index < 3; index++) {
+    basic.showLeds(`
+    . . # . .
+    . # # . .
+    # # # # #
+    . # # . .
+    . . # . .
+    `)
+    basic.clearScreen()
+    basic.pause(500)
+}
+basic.showLeds(`
+. . # . .
+. . # . .
+# # # # #
+. # # # .
+. . # . .
+`)
+basic.clearScreen()
+basic.pause(500)
+basic.showLeds(`
+. . # . .
+. # # . .
+# # # # #
+. # # . .
+. . # . .
+`)
+basic.clearScreen()
+basic.pause(500)
+for (let index = 0; index < 2; index++) {
+    basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+    basic.clearScreen()
+    basic.pause(500)
+}
+for (let index = 0; index < 2; index++) {
+    basic.showLeds(`
+    . . # . .
+    . . # # .
+    # # # # #
+    . . # # .
+    . . # . .
+    `)
+    basic.clearScreen()
+    basic.pause(500)
+}
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.clearScreen()
+basic.pause(500)
+basic.showLeds(`
+. . # . .
+. . # # .
+# # # # #
+. . # # .
+. . # . .
+`)
+basic.clearScreen()
+basic.pause(500)
+basic.showLeds(`
+. . # . .
+. # # # .
+# # # # #
+. . # . .
+. . # . .
+`)
+basic.clearScreen()
+basic.pause(500)
+for (let index = 0; index < 3; index++) {
+    basic.showLeds(`
+    . . # . .
+    . # # . .
+    # # # # #
+    . # # . .
+    . . # . .
+    `)
+    basic.clearScreen()
+    basic.pause(500)
+}
+
